40fa0c1bd5f2f3e6f58a3590b0e90b0b99b4715f
Chris Riesbeck <chris.riesbeck@gmail.com>
2015-04-08 10:18:00 -0500

    add data to save)

--- public/nikoboard.js
+++ public/nikoboard.js
@@ -1,13 +1,13 @@
 function submitMood() {
-  var Reading = Parse.Object.extend("reading");
-  var reading = new Reading();
   var team = $("input:radio[name=team]:checked").val();
   var netid = $("input[name=netid]").val();
   var mood = $("input:radio[name=mood]:checked").val()
   if (team && netid && mood) {
+    var Reading = Parse.Object.extend("reading");
+    var reading = new Reading();
     var data = { team: team, netid: netid, mood: mood };
-    reading.save().then(function (obj) {
-      console.log("saved " + obj);
+    reading.save(data).then(function (obj) {
+      console.log(obj);
     }, function (err) {
       console.log(err);
     });
